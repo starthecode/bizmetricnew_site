983a83fb136f2bd2373691146b9d9befbc991c2a
Madan Malhotra <starthecode@gmail.com>
2025-09-10 22:02:59 +0530

    updating static images

--- frontend/src/components/BackgroundSection.jsx
+++ frontend/src/components/BackgroundSection.jsx
@@ -8,7 +8,7 @@ export default function BackgroundSection({
     <section
       className={`relative py-20 sm:py-24 md:py-24 lg:py-20 z-10 ${className}`}
       style={{
-        backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.6)), url(${url}/uploads/background-colour-gradient-667564.webp)`,
+        backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.6)), url(/assets/imgs/background-colour-gradient-667564.webp)`,
         backgroundSize: 'cover',
         backgroundPosition: 'center center',
         backgroundRepeat: 'no-repeat',
